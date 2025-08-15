f58f6894e661cedf0901a2a57276a942806c5ee4
Monika Chrzan <monika.chrzan9310@gmail.com>
2025-08-15 14:43:55 +0900

    updated rootpath

--- Gemfile
+++ Gemfile
@@ -63,3 +63,10 @@ end
 gem "ostruct"
 gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
+gem "bootstrap", "~> 5.2"
+gem "autoprefixer-rails"
+gem "font-awesome-sass", "~> 6.1"
+gem "simple_form"
+gem "sassc-rails"
+gem 'dotenv-rails', groups: [:development, :test]
+gem 'cloudinary', '~> 1.16.0'
---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,31 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @list = List.find(params[:list_id])
+    if @bookmark.destroy
+      redirect_to list_path(@list)
+    else
+      render 'lists/show', status: :unprocessable_content
+    end
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
